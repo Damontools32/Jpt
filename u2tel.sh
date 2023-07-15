446064edd4f96e7135cd7f63c35a77a971fa1b84
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-15 15:00:36 +0330

    Create u2tel.sh

---
+++ u2tel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update system packages
+sudo apt update
+
+# Install git
+apt-get install git
+
+# Install Python3 and pip
+sudo apt install python3 python3-pip
+
+# Install telethon
+pip3 install telethon
+
+# Clone the GitHub repository
+git clone https://github.com/Damontools32/u2tel/
+
+# Navigate into the cloned directory
+cd u2tel
+
+# Open the Python script in nano for editing
+nano upload_to_telegram_bot.py
